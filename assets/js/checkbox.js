f1c774fb5a930868627c657abc8d10aaa302b2be
yumi-tokuo <yumitokuo@gmail.com>
2021-03-17 00:52:19 -0300

    teste

--- assets/js/checkbox.js
+++ assets/js/checkbox.js
@@ -1,9 +1,17 @@
 function toggleTheme() { 
     var theme = document.getElementsByTagName('link')[0]; 
+    var image = document.getElementsByTagName('link')[0];
 
     if (theme.getAttribute('href') == 'assets/css/main.css') { 
       theme.setAttribute('href', 'assets/css/dark-main.css'); 
     } else { 
       theme.setAttribute('href', 'assets/css/main.css'); 
     } 
+
+    if (image.getAttribute('href') == 'images/yumissart.png') { 
+      image.setAttribute('href', 'images/yumissart-dark.png'); 
+    } else { 
+      image.setAttribute('href', 'images/yumissart.png'); 
+    } 
+
 }
